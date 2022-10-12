965e41d2e65362133a7d5e3d7a54d0096c320286
Hizkiel-Mesfin <mesfinezekiel@gmail.com>
2022-10-12 15:28:52 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import React from "react"
+import React from "react";
+import "./App.css";
 
-function App() {
+function App(props) {
+  const { bankName, num, valid, name } = props;
+
   return (
-    <div className="App">
-     <h1>My React App</h1>
+    <div className="big-con">
+      <div className="card">
+        <h1 className="bank-name"> {bankName} </h1>
+        <p className="card-num"> {num} </p>
+        <p className="valid">Valid until: {valid} </p>
+        <p className="holder-name"> {name} </p>
+      </div>
     </div>
   );
 }
 
-export default App;
+function Card() {
+  return (
+    <App
+      bankName="Sparkasse"
+      num="1234 6543 9876 8678"
+      valid="12/2025"
+      name="Ezekiel Mesfin"
+    />
+  );
+}
+
+export default Card;
